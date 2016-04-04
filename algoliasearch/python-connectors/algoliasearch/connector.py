0a7b0c7950c5f36298c320f7f20e2376311178b6
Jean-Baptiste Rouquier <jrouquie@example.com>
2016-04-04 10:33:49 +0200

    algolia: minor wording

--- algoliasearch/python-connectors/algoliasearch/connector.py
+++ algoliasearch/python-connectors/algoliasearch/connector.py
@@ -121,7 +121,7 @@ class AlgoliaSearchConnectorWriter(CustomDatasetWriter):
         for (col, val) in zip(self.dataset_schema["columns"], row):
             #logging.info("Write %s for %s" % (val, col))
             if len(unicode(val)) > 8000:   #algolia.com/doc/faq/basics/is-there-a-size-limit-for-my-index-records : 10KB
-                val = unicode(val)[0:8000]
+                val = unicode(val)[0:7995] + '(...)'
             if col['type'] in ['tinyint', 'smallint', 'int', 'bigint']:
                 try:
                     val = int(val)
