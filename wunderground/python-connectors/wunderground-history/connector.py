4fd3dae610bde9c7cba6f46aa7bc7f10f7016ae6
Jeremy <jeremy.greze@dataiku.com>
2016-07-12 22:10:54 +0200

    wunderground: api limitations, better results

--- wunderground/python-connectors/wunderground-history/connector.py
+++ wunderground/python-connectors/wunderground-history/connector.py
@@ -12,13 +12,18 @@ class MyConnector(Connector):
     def __init__(self, config):
         Connector.__init__(self, config)
 
-        self.api_key = self.config.get("api_key")
-        self.location = self.config.get("location")
-        self.from_date = self.config.get("from_date")
-        self.to_date = self.config.get("to_date")
-        self.cache_folder = self.config.get("cache_folder")
+        self.api_key = str(self.config.get("api_key"))
+        self.location = str(self.config.get("location"))
+        self.from_date = str(self.config.get("from_date"))
+        self.to_date = str(self.config.get("to_date"))
+        self.calls_per_minute = int(self.config.get("calls_per_minute"))
+        self.api_limit = int(self.config.get("api_limit", 500))
+        self.cache_folder = str(self.config.get("cache_folder", "/tmp/dss-plugin-wunderground"))
+
+        # Cache file path
         filename = "cache-wunderground-history-%s.json" % base64.urlsafe_b64encode(self.location)
         self.cache_file = os.path.join(self.cache_folder, filename)
+        print  "Wunderground plugin - Cache file: %s" % self.cache_file
 
         # Cache directory
         if not os.path.isdir(self.cache_folder):
@@ -30,16 +35,20 @@ class MyConnector(Connector):
                 json.dump({}, f)
                 f.close()
 
+        # Limit on the number of API calls. At each sample refresh, set to 0 again!
+        self.api_calls = 0
+
 
     def get_read_schema(self):
 
-        return None
-
-        # return {
-        #         "columns" : [
-        #             { "name" : "json", "type" : "json" }
-        #         ]
-        #     }
+        return {
+                "columns" : [
+                    { "name" : "day", "type" : "string" },
+                    { "name" : "day_date", "type" : "date" },
+                    { "name" : "daily_summary", "type" : "object" },
+                    { "name" : "observations", "type" : "object" }
+                ]
+            }
 
 
     def get_weather(self, day=None):
@@ -65,16 +74,25 @@ class MyConnector(Connector):
             # Not in cache -> API request
             print  "Wunderground plugin - Request for %s" % day_key
 
+            # checking limits
+            if self.api_limit > -1 and self.api_calls >= self.api_limit:
+                print "/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\/!\\"
+                print "Wunderground plugin - Limit reached (cur=%d lim=%d)" % (self.api_calls, self.api_limit)
+                return {}
+            self.api_calls += 1
+            print "Wunderground plugin - API calls: %i" % self.api_calls
+
             r = requests.get('https://api.wunderground.com/api/' + self.api_key + '/history_' + day.strftime("%Y%m%d") + '/q/' + self.location + '.json')
-            
+
             # verification of the status code
             if r.status_code != 200:
                 print "Wunderground plugin - Error in request (status code: %s)" % r.status_code
+                print "Wunderground plugin - Response: %s" % r.text
                 r.raise_for_status()
                 sys.exit()
 
             result = r.json()
-            print "Wunderground plugin - API response: %s" % json.dumps(result)
+            #print "Wunderground plugin - API response: %s" % json.dumps(result)
 
             # verification of the json response
             if 'response' in result and 'error' in result.get('response'):
@@ -82,7 +100,7 @@ class MyConnector(Connector):
             if 'history' not in result:
                 raise IOError("No history found for %s. Are you sure you found a unique location?" % day_key)
 
-            result = result.get('history').get('observations')
+            result = result.get('history')
 
             # writing json cache
             cache[day_key] = result
@@ -90,7 +108,12 @@ class MyConnector(Connector):
                 json.dump(cache, f)
                 f.close()
 
-            sleep(6)
+            # naive limitation on the rate limit (do not consider the request duration)
+            if self.calls_per_minute <= 10:
+                sleep(6)
+            elif self.calls_per_minute <= 100:
+                sleep(0.6)
+
             return result
 
 
@@ -113,25 +136,12 @@ class MyConnector(Connector):
         # TODO
 
         # Requests
-        LIMIT = 5 #for debug
-        i = 0
         for day in list_datetimes:
-            if i >= LIMIT:
-                print "Wunderground plugin - Limit per execution reached"
-                break
-            else:
-                i += 1
-                result = self.get_weather(day)
-                yield {'day':day.strftime("%d/%m/%Y"),'json':json.dumps(result)}
-
-            #### old stuff
-            #elif d.strftime("%Y-%m-%d") not in existing_dates:
-            #    print  "Wunderground plugin - Request for %s" % d.strftime("%d/%m/%Y")
-            #    i += 1
-
-            #else:
-            #    log( "Already data %s" % d.strftime("%d/%m/%Y"))
-
-
-
+            result = self.get_weather(day)
+            yield {
+                'day': day.strftime("%d/%m/%Y"),
+                'day_date': day.strftime("%Y-%m-%dT00:00:00.000Z"),
+                'daily_summary': json.dumps(result.get('dailysummary')[0]) if 'dailysummary' in result else '',
+                'observations': json.dumps(result.get('observations')) if 'observations' in result else ''
+            }
         
