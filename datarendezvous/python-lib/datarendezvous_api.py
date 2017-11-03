5580f4076594e6959be15f8379eefc548d5e6fd9
pmi <bob@ikea.com>
2017-11-03 11:36:06 +0100

    async data insertion + exception

--- datarendezvous/python-lib/datarendezvous_api.py
+++ datarendezvous/python-lib/datarendezvous_api.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import time
 
 class DrdvApi():
     
@@ -10,7 +11,10 @@ class DrdvApi():
     DATASET_VERSION_ENDPOINT = BASE_URI + '/datasets/%(dataset_id)s/version'
     DATASET_INSERT_ENDPOINT = BASE_URI + '/datasets/%(dataset_id)s/data'
     DATASET_EXPORT_ENDPOINT = BASE_URI + '/datasets/%(dataset_id)s/export'
+    DATASET_STATUS_ENDPOINT = BASE_URI + '/datasets/%(dataset_id)s/status'
     UPLOAD_FILE_NAME = "dataiku.csv"
+    
+    MAX_RETRY = 15
     
     
     def __init__(self, api_key):
@@ -43,7 +47,7 @@ class DrdvApi():
         payload['datapot_id'] = datapot_id
         payload['file_key'] = file_key
         response = self.session.put(self.DATASET_ENDPOINT, json=payload, headers=self._headers())
-        return response.json()['dataset_id']
+        return self._waitFor(response.json()['dataset_id'])
     
     def new_dataset_version(self, datapot_id, dataset_id, file_key):
         
@@ -52,12 +56,28 @@ class DrdvApi():
         payload['datapot_id'] = datapot_id
         payload['file_key'] = file_key
         response = self.session.put(self.DATASET_VERSION_ENDPOINT % {"dataset_id": dataset_id}, json=payload, headers=self._headers())
-        return response.json()['dataset_id']
+        return self._waitFor(response.json()['dataset_id'])
     
     def dataset_insert(self, dataset_id, records):
-        
         payload = {}
         payload['records'] = records
         response = self.session.post(self.DATASET_INSERT_ENDPOINT % {"dataset_id": dataset_id}, json=payload, headers=self._headers())
         response.raise_for_status()
+        
+    def dataset_status(self, dataset_id):
+        response = self.session.get( self.DATASET_STATUS_ENDPOINT % {"dataset_id": dataset_id} , headers = self._headers())
+        return response.json()['status']
+    
+    def _waitFor(self, dataset_id):
+        retry = 0
+        status = self.dataset_status(dataset_id)
+        while status == 'pending' and retry < self.MAX_RETRY:
+            time.sleep(1)
+            status = self.dataset_status(dataset_id)
+            retry += 1
+        if status == 'error' or retry == self.MAX_RETRY:
+            raise Exception('An error occured while initializing dataset %s' % dataset_id)
+        return dataset_id
+        
+        
     
