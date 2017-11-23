10b5ada258fbaaf840acbb47bc4d4ada81828f92
Thomas Labadie <thomas.labadie@gmail.com>
2017-11-23 11:22:16 +0100

    Updates: - added the ability to compare non-numerical datasets - improved the computation time by nearly over 9000 - logs and does not crash when no value columns are selected

--- compare-datasets/custom-recipes/compute-difference/recipe.py
+++ compare-datasets/custom-recipes/compute-difference/recipe.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
+import logging
+
+from itertools import islice
 from dataiku import pandasutils as pdu
+from pandas.api.types import is_numeric_dtype
 from dataiku.customrecipe import *
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 # Getting inputs & outputs
 input_first_ds_name = get_input_names_for_role('first_dataset')[0]
@@ -25,9 +31,25 @@ input_second_df = input_second_ds.get_dataframe()
 
 # Performing an outer join on the key columns
 output = input_first_df.merge(input_second_df, on=key_columns, how='outer')
-for value_column in value_columns:  
-    output[value_column + '_diff']= output.apply(lambda row: abs(row[value_column + '_x'] - row[value_column + '_y']), axis=1)
-    output[value_column + '_isok']= output.apply(lambda row: row[value_column + '_diff'] < tolerance, axis=1)
+
+if value_columns:
+    for value_column in value_columns:
+        if (is_numeric_dtype(output[value_column + '_x']) and is_numeric_dtype(output[value_column + '_y'])):
+            output[value_column + '_diff'] = output[value_column + '_x'].subtract(output[value_column + '_y']).abs()
+            output[value_column + '_isok'] = output[value_column + '_diff'].lt(tolerance)
+        else:
+            output[value_column + '_isok'] = output[value_column + '_x'].eq(output[value_column + '_y'])
+
+    isok_summary = output[value_columns[0] + '_isok']
+    for value_column in islice(value_columns, 1, None):
+        isok_summary &= output[value_column + '_isok']
+            
+    output.insert(loc=0, column='isok_summary', value=isok_summary)
+
+
+else:
+    logging.info("No value columns selected for comparison; only performing a merge")
+
 
 
 # Recipe outputs
