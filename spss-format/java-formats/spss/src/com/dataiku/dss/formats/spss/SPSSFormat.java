ef27339d344f013525c40d816e09615a4b16517a
Thomas Labadie <thomas.labadie@gmail.com>
2017-12-11 11:40:34 +0100

    SPSS format reader: - Little/big endian & compression support by @JoachimZ - Dates turned into ISO8601 format - Long strings (>255) support - Encoding support - Recognizing SPSS missing values - Various fixes, including regarding specific records reading - Unit testing added

---
+++ spss-format/java-formats/spss/src/com/dataiku/dss/formats/spss/SPSSFormat.java
@@ -0,0 +1,84 @@
+package com.dataiku.dss.formats.spss;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.dataiku.dip.coremodel.Schema;
+import com.dataiku.dip.coremodel.SchemaColumn;
+import com.dataiku.dip.datalayer.Column;
+import com.dataiku.dip.datalayer.ColumnFactory;
+import com.dataiku.dip.datalayer.ProcessorOutput;
+import com.dataiku.dip.datalayer.Row;
+import com.dataiku.dip.datalayer.RowFactory;
+import com.dataiku.dip.plugin.CustomFormat;
+import com.dataiku.dip.plugin.CustomFormatSchemaDetector;
+import com.dataiku.dip.plugin.CustomFormatInput;
+import com.dataiku.dip.plugin.CustomFormatOutput;
+import com.dataiku.dip.plugin.InputStreamWithFilename;
+import com.dataiku.dip.warnings.WarningsContext;
+import com.google.gson.JsonObject;
+
+public class SPSSFormat implements CustomFormat {
+    /**
+     * Create a new instance of the format
+     */
+    public SPSSFormat() {
+    }
+    
+    /**
+     * Create a reader for a stream in the format
+     */
+    @Override
+    public CustomFormatInput getReader(JsonObject config, JsonObject pluginConfig) {
+        return new SPSSFormatInput();
+    }
+
+    /**
+     * Create a writer for a stream in the format
+     */
+    @Override
+    public CustomFormatOutput getWriter(JsonObject config, JsonObject pluginConfig) {
+        throw new UnsupportedOperationException("No output for this format.");
+    }
+    
+    /**
+     * Create a schema detector for a stream in the format (used if canReadSchema=true in the json)
+     */
+    @Override
+    public CustomFormatSchemaDetector getDetector(JsonObject config, JsonObject pluginConfig) {
+        throw new UnsupportedOperationException("No detector for this format.");
+    }
+
+    public static class SPSSFormatInput implements CustomFormatInput {
+        private WarningsContext wc;
+
+        /**
+         * Called if the schema is available (ie, dataset has been created)
+         */
+        @Override
+        public void setSchema(Schema schema, boolean allowExtraColumns) {
+        }
+
+        @Override
+        public void setWarningsContext(WarningsContext warnContext) {
+            this.wc = warnContext;
+        }
+
+        /**
+         * extract data from the input stream. The emitRow() on the out will throw exceptions to 
+         * enforce limits set to number of rows read, so these should not be caught and hidden.
+         */
+        @Override
+        public void run(InputStreamWithFilename in, ProcessorOutput out, ColumnFactory cf, RowFactory rf) throws Exception {
+            SPSSStreamReader reader = new SPSSStreamReader(in.getInputStream(), cf, wc);
+            reader.readData(out, rf);
+        }
+
+        @Override
+        public void close() throws IOException {
+        }
+    }
+
+}
+
