d6cf814c0c8ac4b9e02ee4ef9f0bc1f89b893994
Thomas Labadie <thomas.labadie@gmail.com>
2017-12-26 13:26:32 +0100

    SAS: Updated makefile, fixed column order

--- sas-format/python-formats/sas/format.py
+++ sas-format/python-formats/sas/format.py
@@ -1,5 +1,7 @@
 from dataiku.customformat import Formatter, OutputFormatter, FormatExtractor
 from dataiku.base.java_link import LinkedInputStream
+from collections import OrderedDict
+from distutils.version import LooseVersion
 
 import pandas as pd
 import json, base64
@@ -15,6 +17,7 @@ class SASFormatter(Formatter):
     def get_format_extractor(self, stream, schema=None):
         return SASFormatExtractor(stream, schema, self.config)    
 
+    
 # Fix for the stream class provided by DSS
 # Seek could be disabled by a one-liner like the following one but read_sas may seek forward
 # self.stream.seek = types.MethodType(lambda self, _: False, self.stream)
@@ -45,8 +48,18 @@ class SASFormatExtractor(FormatExtractor):
         chunksize = int(config.get("chunksize", "10000"))
         
         self.iterator = pd.read_sas(ForwardSeekStream(stream), format="sas7bdat", iterator=True, chunksize=chunksize)
+        self.is_pandas_021 = LooseVersion(pd.__version__) >= LooseVersion('0.21.0')
+        self.get_ordered_chunk()
+
+    # OrderedDict is needed to preserve the column order, but the
+    # `into` parameter was only introduced in Pandas 0.21.0
+    def get_ordered_chunk(self):
+        if (self.is_pandas_021):
+            self.chunk = next(self.iterator).to_dict('records', into=OrderedDict)
+        else:
+            self.chunk = [OrderedDict(row) for i, row in next(self.iterator).iterrows()]
+        
         self.chunk_nb = 0
-        self.chunk = next(self.iterator).to_dict('records')
         
     def read_schema(self):
         return [{"name": c.name, "type": "DOUBLE" if c.ctype == 'd' else "STRING"} for c in self.iterator.columns]
@@ -54,8 +67,7 @@ class SASFormatExtractor(FormatExtractor):
     def read_row(self):
         try:
             if self.chunk_nb >= len(self.chunk): 
-                self.chunk = next(self.iterator).to_dict('records')
-                self.chunk_nb = 0
+                self.get_ordered_chunk()
                 
             self.chunk_nb += 1
             return self.chunk[self.chunk_nb - 1]
