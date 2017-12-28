d02b85f091443e4113e1d28def17ad71cebe1f9a
Thomas Labadie <thomas.labadie@gmail.com>
2017-12-28 12:09:58 +0100

    SAS: Updated format for proper previewing on DSS < 4.1.X

--- sas-format/python-formats/sas/format.py
+++ sas-format/python-formats/sas/format.py
@@ -1,7 +1,6 @@
 from dataiku.customformat import Formatter, OutputFormatter, FormatExtractor
 from dataiku.base.java_link import LinkedInputStream
 from collections import OrderedDict
-from distutils.version import LooseVersion
 
 import pandas as pd
 import json, base64
@@ -17,7 +16,7 @@ class SASFormatter(Formatter):
     def get_format_extractor(self, stream, schema=None):
         return SASFormatExtractor(stream, schema, self.config)    
 
-    
+
 # Fix for the stream class provided by DSS
 # Seek could be disabled by a one-liner like the following one but read_sas may seek forward
 # self.stream.seek = types.MethodType(lambda self, _: False, self.stream)
@@ -46,19 +45,17 @@ class SASFormatExtractor(FormatExtractor):
         FormatExtractor.__init__(self, stream)
         
         chunksize = int(config.get("chunksize", "10000"))
-        
+        self.hasSchema = schema != None
         self.iterator = pd.read_sas(ForwardSeekStream(stream), format="sas7bdat", iterator=True, chunksize=chunksize)
-        self.is_pandas_021 = LooseVersion(pd.__version__) >= LooseVersion('0.21.0')
-        self.get_ordered_chunk()
-
-    # OrderedDict is needed to preserve the column order, but the
-    # `into` parameter was only introduced in Pandas 0.21.0
-    def get_ordered_chunk(self):
-        if (self.is_pandas_021):
-            self.chunk = next(self.iterator).to_dict('records', into=OrderedDict)
-        else:
-            self.chunk = [OrderedDict(row) for i, row in next(self.iterator).iterrows()]
+        self.get_chunk()
         
+    def get_chunk(self):
+        # Fix for previewing when using DSS < 4.1.X
+        if self.hasSchema:
+            self.chunk = next(self.iterator).to_dict('records')
+        else:
+            self.chunk = [OrderedDict(row) for i, row in next(self.iterator).iterrows()]    
+            
         self.chunk_nb = 0
         
     def read_schema(self):
@@ -67,7 +64,7 @@ class SASFormatExtractor(FormatExtractor):
     def read_row(self):
         try:
             if self.chunk_nb >= len(self.chunk): 
-                self.get_ordered_chunk()
+                self.get_chunk()
                 
             self.chunk_nb += 1
             return self.chunk[self.chunk_nb - 1]
