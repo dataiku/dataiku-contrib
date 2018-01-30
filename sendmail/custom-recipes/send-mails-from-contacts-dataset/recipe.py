8fdd9b09f45d837f57a21d5608411899df288a9a
Thomas Labadie <thomas.labadie@gmail.com>
2018-01-30 18:14:56 +0100

    Sendmail: various fixes, improved UI

--- sendmail/custom-recipes/send-mails-from-contacts-dataset/recipe.py
+++ sendmail/custom-recipes/send-mails-from-contacts-dataset/recipe.py
@@ -23,28 +23,31 @@ attachments = [dataiku.Dataset(x) for x in get_input_names_for_role('attachments
 config = get_recipe_config()
 
 recipient_column = config.get('recipient_column', None)
-recipient_value = config.get('recipient_value', None)
+
 sender_column = config.get('sender_column', None)
 sender_value = config.get('sender_value', None)
+use_sender_value = config.get('use_sender_value', False)
+
 subject_column = config.get('subject_column', None)
 subject_value = config.get('subject_value', None)
+use_subject_value = config.get('use_subject_value', False)
+
 body_column = config.get('body_column', None)
 body_value = config.get('body_value', None)
+use_body_value = config.get('use_body_value', False)
 
 smtp_host = config.get('smtp_host', None)
 smtp_port = int(config.get('smtp_port', "25"))
 
 attachment_type = config.get('attachment_type', "csv")
 
-
 output_schema = list(people.read_schema())
-output_schema.append({'name':'STATUS', 'type':'STRING'})
-output_schema.append({'name':'mailsend_error', 'type':'STRING'})
+output_schema.append({'name':'sendmail_status', 'type':'string'})
+output_schema.append({'name':'sendmail_error', 'type':'string'})
 output.write_schema(output_schema)
 
 if not body_column and not body_value:
-    raise Exception("NO body column nor body value specified")
-
+    raise AttributeError("No body column nor body value specified")
 
 # Prepare attachements
 mime_parts = []
@@ -75,23 +78,11 @@ s = smtplib.SMTP(smtp_host, port=smtp_port)
 
 
 def send_email(contact):
-    if recipient_value:
-        recipient = recipient_value
-    elif recipient_column:
-        recipient = contact[recipient_column]
-    if body_value:
-        email_text = body_value
-    elif body_column:
-        email_text = contact[body_column]
-    if subject_value:
-        email_subject = subject_value
-    elif subject_column:
-        email_subject = contact[subject_column]
-    if sender_value:
-        sender = sender_value
-    elif sender_column:
-        sender = contact[sender_column]
-
+    recipient = contact[recipient_column]
+    email_text = body_value if use_body_value else contact[body_column]
+    email_subject = subject_value if use_subject_value else contact[subject_column]
+    sender = sender_value if use_sender_value else contact[sender_column]
+    
     msg = MIMEMultipart()
 
     msg["From"] = sender
@@ -114,7 +105,7 @@ with output.get_writer() as writer:
         try:
             send_email(contact)
             d = dict(contact)
-            d['STATUS'] = 'SUCCESS'
+            d['sendmail_status'] = 'SUCCESS'
             success+=1
             if writer:
                 writer.write_row_dict(d)
@@ -122,8 +113,8 @@ with output.get_writer() as writer:
             logging.exception("Send failed")
             fail+=1
             d = dict(contact)
-            d['STATUS'] = 'FAILED'
-            d['mailsend_error'] = str(e)
+            d['sendmail_status'] = 'FAILED'
+            d['sendmail_error'] = str(e)
             if writer:
                 writer.write_row_dict(d)
 
