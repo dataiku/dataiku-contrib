809d05bf1dd8ca4b48338ff366d1d8acf82494c7
Raphael Daguenet <raphael.daguenet@gmail.com>
2018-06-01 13:18:32 +0200

    Add `payloadMaxSize` config param

--- algoliasearch/python-connectors/algoliasearch/connector.py
+++ algoliasearch/python-connectors/algoliasearch/connector.py
@@ -77,7 +77,7 @@ class AlgoliaSearchConnector(Connector):
         index = self._get_index()
         res = index.search(self.config.get("searchQuery", ""), search_settings)
 
-        vals =[]
+        vals = []
 
         for dim in dataset_partitioning["dimensions"]:
             facet = res["facets"][dim["name"]]
@@ -106,6 +106,7 @@ class AlgoliaSearchConnectorWriter(CustomDatasetWriter):
         self.dataset_partitioning = dataset_partitioning
         self.partition_id = partition_id
         self.batch_size = int(config["batchSize"])
+        self.payload_max_size = int(config["payloadMaxSize"])
 
         self.buffer = []
 
@@ -120,6 +121,12 @@ class AlgoliaSearchConnectorWriter(CustomDatasetWriter):
     def write_row(self, row):
         obj = {}
         for (col, val) in zip(self.dataset_schema["columns"], row):
+            # Truncate cell value to fit Algolia's record size limit. Skipped by default.
+            # See https://www.algolia.com/doc/faq/basics/is-there-a-size-limit-for-my-index-records/
+            if self.payload_max_size > 0 and len(unicode(val)) > (self.payload_max_size - 2000):
+                logging.warning("Algolia payload max size reached, truncating record")
+                max_size = self.payload_max_size - 5
+                val = unicode(val)[0:max_size] + '(...)'
             if col['type'] in ['tinyint', 'smallint', 'int', 'bigint']:
                 try:
                     val = int(val)
@@ -148,7 +155,7 @@ class AlgoliaSearchConnectorWriter(CustomDatasetWriter):
                 logging.info("Forcing partitioning dim: %s=%s" % (dim["name"], id_chunks[idx]))
                 idx += 1
 
-        logging.info("Final obj: %s" % obj)
+        logging.debug("Final obj: %s" % obj)
         self.buffer.append(obj)
 
         if len(self.buffer) >= self.batch_size:
