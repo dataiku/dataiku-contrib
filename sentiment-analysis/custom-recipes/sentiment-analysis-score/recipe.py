187d81ed573f1ac8affd994bb4ffaaceef243fad
helboukkouri <36409068+helboukkouri@users.noreply.github.com>
2018-08-13 11:15:08 +0200

    Add chunk processing
    Updated the recipe so that it scores the input data in chunks, therefore avoiding OOM errors.

--- sentiment-analysis/custom-recipes/sentiment-analysis-score/recipe.py
+++ sentiment-analysis/custom-recipes/sentiment-analysis-score/recipe.py
@@ -10,13 +10,24 @@ from preprocessing_utils import clean_text
 import numpy as np
 from fastText import load_model
 
+#############################
+# Logging Settings
+#############################
+
+import logging
+
+FORMAT = '[PLUGIN RECIPE LOG] %(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 #############################
 # Input data
 #############################
 
-input_dataset = get_input_names_for_role('input_dataset')[0]
-df = dataiku.Dataset(input_dataset).get_dataframe()
+input_dataset_name = get_input_names_for_role('input_dataset')[0]
+input_dataset = dataiku.Dataset(input_dataset_name)
 
 
 #############################
@@ -25,25 +36,18 @@ df = dataiku.Dataset(input_dataset).get_dataframe()
 
 recipe_config = get_recipe_config()
 
-
 text_column_name = recipe_config.get('text_column_name', None)
 if text_column_name == None:
     raise ValueError("You did not choose a text column.")
-
-    
-texts = df[text_column_name].apply(
-    lambda s: clean_text(str(s)).decode('utf-8')).values
-    
-text_language = "english"
 
 output_probabilities = bool(recipe_config.get('output_confidence', False))
 
 
 #############################
-# Load Models
+# Load FastText Model
 #############################
 
-en_model = load_model(
+model = load_model(
     os.path.join(
         get_recipe_resource(),
         "fasttext",
@@ -57,39 +61,56 @@ en_model = load_model(
 # Score
 #############################
 
-model = en_model
+CHUNK_SIZE = 10000
 
-predicted_polarities, confidence_list = model.predict(list(texts))
-predicted_polarities = np.array([int(v[0].split('__')[-1]) for v in predicted_polarities])
-if text_language == "english":
+# Output Dataset
+dataset_name = get_output_names_for_role('output_dataset')[0]
+output_dataset = dataiku.Dataset(dataset_name)
+
+logger.info("Started chunk-processing of input Dataset.".format(dataset_name))
+
+n_lines = 0
+for chunk_idx, df in enumerate(input_dataset.iter_dataframes(chunksize=CHUNK_SIZE)):
+
+    # Clean texts
+    texts = df[text_column_name].apply(lambda s: clean_text(str(s)).decode('utf-8')).values
+
+    # Predict Sentiment
+    predicted_polarities, confidence_list = model.predict(list(texts))
+
+    # Post-process predicted Sentiment
+    predicted_polarities = np.array([int(v[0].split('__')[-1]) for v in predicted_polarities])
     predicted_polarities += -1  # English model predicts 1/2 instead of 0/1
+    confidence_list = confidence_list.ravel()
 
-confidence_list = confidence_list.ravel()
+    if chunk_idx==0:
 
+        # Compute new column names
+        new_cols = ["predicted_polarity", "predicted_sentiment"]
+        if output_probabilities:
+            new_cols.append("prediction_confidence")
 
-# Add new columns to output dataset
+        for i, column in enumerate(new_cols):
+            if column in df.columns:
+                j = 1
+                while column + "_{}".format(j) in df.columns:
+                    j += 1
+                new_cols[i] = column + "_{}".format(j)
 
-new_cols = ["predicted_polarity", "predicted_sentiment"]
-if output_probabilities:
-    new_cols.append("prediction_confidence")
+    # Add prediction to output dataframe
+    df[new_cols[0]] = predicted_polarities
+    df[new_cols[1]] = ["positive" if p==1 else "negative" for p in predicted_polarities]
+    if output_probabilities:
+        df[new_cols[2]] = np.array(confidence_list) / float(max(confidence_list))
 
-for i, column in enumerate(new_cols):
-    if column in df.columns:
-        j = 1
-        while column + "_{}".format(j) in df.columns:
-            j += 1
-        new_cols[i] = column + "_{}".format(j)
-
-df[new_cols[0]] = predicted_polarities
-df[new_cols[1]] = ["positive" if p==1 else "negative" for p in predicted_polarities]
-if output_probabilities:
-    df[new_cols[2]] = np.array(confidence_list) / float(max(confidence_list))
-
-
-#############################
-# Output
-#############################
-
-output_dataset = get_output_names_for_role('output_dataset')[0]
-dataiku.Dataset(output_dataset).write_with_schema(df)
-
+    # Append dataframe to output Dataset
+    if chunk_idx==0:
+        output_dataset.write_schema_from_dataframe(df)
+        writer = output_dataset.get_writer()
+        writer.write_dataframe(df)
+    else:
+        writer.write_dataframe(df)
+    
+    n_lines += len(df)
+    logger.info("Finished processing {} lines".format(n_lines))
+writer.close()
