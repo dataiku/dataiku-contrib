2cb3f0789b0729bda238c2e20f291c05dcf7b67e
Jean-Bernard Jansen <jean-bernard.jansen@dataiku.com>
2018-11-22 11:18:28 +0100

    disk-cleanup: Add a macro to truncate splits on analysis data too

--- disk-cleanup-macros/python-runnables/truncate-saved-models-data/runnable.py
+++ disk-cleanup-macros/python-runnables/truncate-saved-models-data/runnable.py
@@ -6,6 +6,7 @@ import os, shutil, os.path as osp
 import datetime, time
 import json
 import cleanup
+import logging
 
 class MyRunnable(Runnable):
     def __init__(self, project_key, config, plugin_config):
@@ -39,6 +40,11 @@ class MyRunnable(Runnable):
         rt.add_column("kept_splits", "Kept splits", "STRING")
         rt.add_column("truncated_splits", "Truncated splits", "STRING")
         rt.add_column("reclaimed_size", "Reclaimed size", "STRING")
+
+        grand_total_used = 0
+        grand_total_reclaimed = 0
+        grand_total_kept = 0
+        grand_total_deleted = 0
         
         for project in cleanup.get_projects_to_consider(self.project_key, self.config):
             project_sm = osp.join(saved_models, project)
@@ -56,7 +62,7 @@ class MyRunnable(Runnable):
                 kept_versions = 0
                 deleted_versions = 0
                 size_reclaimed = 0
-                total_size_before = cleanup.du(sm_dir)
+                total_size_before = cleanup.du(sm_dir, size_unit="b")
 
                 for version in os.listdir(versions_dir):
                     version_dir = osp.join(versions_dir, version)
@@ -76,24 +82,37 @@ class MyRunnable(Runnable):
                                             truncate_file(path, lines)
                                             size_reclaimed += initial - os.stat(path).st_size
                                         except Exception as e:
-                                            errors.append(str(e))
+                                            logging.getLogger().error("{}: {}".format(path,str(e)))
                                     else:
                                         size_reclaimed += os.stat(path).st_size
-
                     else:
                         kept_versions += 1
                     
-                total_size_after = cleanup.du(sm_dir)
-
+                total_size_after = cleanup.du(sm_dir,size_unit="b")
                 record = []
                 record.append(project)
                 record.append(saved_model)
                 record.append(saved_model)
-                record.append(total_size_before / 1024)
-                record.append(total_size_after / 1024)
+                record.append(cleanup.format_size(total_size_before))
+                record.append(cleanup.format_size(total_size_after))
                 record.append(kept_versions)
                 record.append(deleted_versions)
-                record.append(size_reclaimed)
+                record.append(cleanup.format_size(size_reclaimed))
                 rt.add_record(record)
 
+                grand_total_reclaimed += size_reclaimed
+                grand_total_used += total_size_before
+                grand_total_kept += kept_versions
+                grand_total_deleted += deleted_versions
+
+        rt.add_record([
+            "Total",
+            "-",
+            "-",
+            cleanup.format_size(grand_total_used), 
+            cleanup.format_size(grand_total_used-grand_total_reclaimed),
+            grand_total_kept,
+            grand_total_deleted,
+            cleanup.format_size(grand_total_reclaimed)
+            ])
         return rt
