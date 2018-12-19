abeda64b1b4146e03b6f56c1d6bd26613268e036
Harizo Rajaona <harizo.rajaona@gmail.com>
2018-12-19 19:16:03 +0100

    Added a few improvements from the private version:
    - option to exclude shared datasets
    - a bit of refactoring
    - logging
    - some aesthetics

---
+++ clear-intermediate-datasets/python-runnables/clear_int_datasets/runnable.py
@@ -0,0 +1,129 @@
+import dataiku
+import logging
+
+from dataiku.runnables import Runnable, ResultTable
+
+logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
+
+def populate_result_table_with_list():
+    # TODO
+    # Populate result table from list with arbitrary table size
+    return NotImplementedError()
+
+
+class MyRunnable(Runnable):
+    """The base interface for a Python runnable."""
+
+    def __init__(self, project_key, config, plugin_config):
+        """
+        :param project_key: the project in which the runnable executes
+        :param config: the dict of the configuration of the object
+        :param plugin_config: contains the plugin settings
+        """
+
+        self.project_key = project_key
+        self.config = config
+        self.plugin_config = plugin_config
+
+    def get_progress_target(self):
+        """
+        If the runnable will return some progress info, have this function return a tuple of 
+        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
+        """
+
+        return None
+
+    def run(self, progress_callback):
+
+        # Retrieve macro parameters:
+        is_dry_run = self.config.get("is_dry_run")
+        keep_partitioned = self.config.get("keep_partitioned")
+        keep_shared = self.config.get("keep_shared")
+        logging.info("DRY RUN is set to {}".format(str(is_dry_run)))
+        logging.info("KEEP PARTITIONED is set to {}".format(str(keep_partitioned)))
+        logging.info("KEEP SHARED is set to {}".format(str(keep_shared)))
+
+        # Initialize macro result table:
+        result_table = ResultTable()
+        result_table.add_column("dataset", "dataset", "STRING")
+        result_table.add_column("status", "status", "STRING")
+
+        client = dataiku.api_client()
+        project = client.get_project(self.project_key)
+        all_datasets = project.list_datasets()
+        all_recipes = project.list_recipes()
+
+        # Build deduplicated lists of input/output datasets:
+        input_datasets = []
+        output_datasets = []
+        for rcp in all_recipes:
+            # List dataset names that are inputs:
+            rcp_in = [x["ref"] for x in rcp["inputs"]["main"]["items"]]
+            # Append them to the overall input list:
+            input_datasets += rcp_in
+            # List dataset names that are outputs:
+            rcp_out = [x["ref"] for x in rcp["outputs"]["main"]["items"]]
+            output_datasets += rcp_out
+        # Deduplicate input/output lists:
+        input_datasets = list(set(input_datasets))
+        output_datasets = list(set(output_datasets))
+
+        # Identify Flow input/outputs & add them to result table:
+        flow_inputs = [x for x in input_datasets if x not in output_datasets]
+        for obj in flow_inputs:
+            result_table.add_record([obj, "KEEP(INPUT)"])
+        flow_outputs = [x for x in output_datasets if x not in input_datasets]
+        for obj in flow_outputs:
+            result_table.add_record([obj, "KEEP(OUTPUT)"])
+        logging.info("Found {} FLOW INPUT datasets: {}".format(str(len(flow_inputs)),
+                                                               str(flow_inputs)))
+        logging.info("Found {} FLOW OUTPUT datasets: {}".format(str(len(flow_outputs)),
+                                                                str(flow_outputs)))
+
+        # Identify shared datasets:
+        shared_objs = project.get_settings().settings["exposedObjects"]["objects"]
+        shared_datasets = [x["localName"] for x in shared_objs if x["type"]=="DATASET"]
+        logging.info("Found {} SHARED datasets: {}".format(str(len(shared_datasets)),
+                                                           str(shared_datasets)))
+
+        # Identify partitioned (partd) datasets:
+        is_partd = lambda x: len(x["partitioning"]["dimensions"]) > 0
+        partd_datasets = [x["name"] for x in all_datasets if is_partd(x)]
+        logging.info("Found {} PARTITIONED datasets: {}".format(str(len(partd_datasets)),
+                                                                str(partd_datasets)))
+
+        # List all datasets to keep, potentially including shared & partd ones:
+        to_keep = flow_inputs + flow_outputs
+        if keep_partitioned:
+            to_keep += partd_datasets
+            # Add them to result table:
+            for obj in partd_datasets:
+                result_table.add_record([obj, "KEEP(PARTITIONED)"])
+        if keep_shared:
+            to_keep += shared_datasets
+            # Add them to result table:
+            for obj in shared_datasets:
+                result_table.add_record([obj, "KEEP(SHARED)"])
+        logging.info("Total of {} datasets to KEEP: {}".format(str(len(to_keep)),
+                                                               str(to_keep)))
+
+        # Perform cleanup or simulate it (dry run):
+        if not is_dry_run:
+            for ds in all_datasets:
+                ds_name = ds["name"]
+                if ds_name not in to_keep:
+                    dataset = project.get_dataset(ds_name)
+                    logging.info("Clearing {}...".format(ds_name))
+                    dataset.clear()
+
+        return result_table
+
+
+
+
+
+
+
+
+
