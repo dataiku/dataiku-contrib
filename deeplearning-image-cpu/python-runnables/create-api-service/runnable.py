18243d129e4715a468c48dca2a97f7cb0cbe5a51
Du Phan <duphan@Du-MacBook.local>
2019-01-24 15:03:06 +0100

    add support for gpu endpoint deployment

--- deeplearning-image-cpu/python-runnables/create-api-service/runnable.py
+++ deeplearning-image-cpu/python-runnables/create-api-service/runnable.py
@@ -36,7 +36,7 @@ class MyRunnable(Runnable):
 
         params = get_params(self.config, self.client, self.project)
         copy_plugin_to_dss_folder(self.plugin_id, params.get("model_folder_id"), self.project_key)
-        create_api_code_env(self.client, params.get('code_env_name'))
+        create_api_code_env(self.client, params.get('code_env_name'), params.get('use_gpu'))
         api_service = get_api_service(params, self.project)            
         endpoint_settings = get_model_endpoint_settings(params)
         create_python_endpoint(api_service, endpoint_settings) 
