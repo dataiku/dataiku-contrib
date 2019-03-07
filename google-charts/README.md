ea841b825ac1299ec4f0c84639afe0a923320a77
Du Phan <duphan@Du-MacBook.local>
2019-03-07 15:43:55 +0100

    waterfall chart webapp

---
+++ google-charts/README.md
@@ -0,0 +1,12 @@
+# Google charts
+
+This Dataiku DSS plugin helps you create, without code, different type of charts.
+
+It contains several webapps for 
+* waterfall/bridge chart.
+
+The charts are based on the [Google Charts library](https://developers.google.com/chart/) and requires internet access.
+
+## License 
+
+This project is licensed under the Apache Software License.
---
+++ google-charts/resource/waterfall-chart.js
@@ -0,0 +1,66 @@
+let dataReady;
+let chartReady;
+
+function initWaterfall(onReady) {
+    try {
+        dataiku.checkWebAppParameters();
+    } catch (e) {
+        webappMessages.displayFatalError(e.message + ' Go to settings tab.');
+        return;
+    }
+    
+    const cfg = dataiku.getWebAppConfig();
+    const dataset = cfg['dataset'];
+    const unit = cfg['unit'];
+    const value = cfg['value'];
+    const sampling = {};
+
+    let allRows;
+    let old_record = null;
+    
+    function transform_record(record){
+        var row = [];
+        row.push(record[unit]);
+        if (old_record == null){
+            row.push("0");
+            row.push("0");
+        }
+        else {
+            row.push(old_record[value]);
+            row.push(old_record[value]);
+        }
+
+        row.push(record[value]);
+        row.push(record[value]);
+
+        old_record = record;
+        return row;
+    }
+
+    function drawAppIfEverythingReady() {
+        if (!chartReady || !dataReady) {
+            return;
+        }
+        onReady(allRows);
+    }
+
+    if (!window.google) {
+        webappMessages.displayFatalError('Failed to load Google Charts library. Check your connection.');
+    } else {
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(function() {
+            chartReady = true;
+            drawAppIfEverythingReady();
+        });
+        dataiku.fetch(dataset, sampling, function(dataFrame) {
+
+            allRows = dataFrame.mapRecords(transform_record);
+
+            var last_index = allRows.length - 1;
+            var final_row = ["final", "0", "0", allRows[last_index][4], allRows[last_index][4]];
+            allRows.push(final_row);
+            dataReady = true;
+            drawAppIfEverythingReady();
+        });
+    }
+}
