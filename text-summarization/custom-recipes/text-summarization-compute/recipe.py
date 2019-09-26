1ee081855ef4b4031f68f772effcde973e119ef9
DamienJacquemart <damien.jacquemart@dataiku.com>
2019-09-26 16:50:08 +0200

    empty and capital fixed
    Empty does not throw error.
    
    If all the text is in capital letters, previous version gave empty output. Fixed.

--- text-summarization/custom-recipes/text-summarization-compute/recipe.py
+++ text-summarization/custom-recipes/text-summarization-compute/recipe.py
@@ -5,6 +5,7 @@ from __future__ import division, print_function, unicode_literals
 
 import dataiku
 from dataiku.customrecipe import *
+import numpy as np
 
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
@@ -55,18 +56,37 @@ elif method == "LSA":
 LANGUAGE = "english"
 
 
+
 def summarize(text):
 
-    parser = PlaintextParser.from_string(text.decode(
-        'ascii', errors='ignore'), Tokenizer(LANGUAGE))
-    stemmer = Stemmer(LANGUAGE)
+    if text == text: #avoid nan
+        
+        all_capital = False
+        
+        # to avoid that all capital letter sentence gives empty output: we lower all and the upper all later on
+        if text.upper() == text:
+            text = text.lower()
+            all_capital = True
+        
+        parser = PlaintextParser.from_string(text.decode(
+            'ascii', errors='ignore'), Tokenizer(LANGUAGE))
+        stemmer = Stemmer(LANGUAGE)
 
-    summarizer = Summarizer(stemmer)
-    summarizer.stop_words = get_stop_words(LANGUAGE)
+        summarizer = Summarizer(stemmer)
+        summarizer.stop_words = get_stop_words(LANGUAGE)
 
-    sentences = [str(s) for s in summarizer(
-        parser.document, sentences_count=n_sentences)]
-    return ' '.join(sentences)
+        sentences = [str(s) for s in summarizer(
+            parser.document, sentences_count=n_sentences)]
+        
+        if all_capital:
+            output_sentences = ' '.join(sentences).upper()
+            all_capital = False
+        else:
+            output_sentences = ' '.join(sentences)
+
+        return output_sentences
+    else:
+        return ''
 
 
 # Checking for existing columns with same name
