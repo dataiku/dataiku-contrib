59098111fa2734e6b43a1256d52a541ace78dc62
Joachim Zentici <joachim.zentici@dataiku.com>
2019-10-16 19:30:33 +0200

    sentiment analysis small improvements

--- sentiment-analysis/custom-recipes/sentiment-analysis-score/recipe.py
+++ sentiment-analysis/custom-recipes/sentiment-analysis-score/recipe.py
@@ -29,6 +29,8 @@ text_column_name = recipe_config.get('text_column_name', None)
 if text_column_name == None:
     raise ValueError("You did not choose a text column.")
 sentiment_scale = recipe_config.get('sentiment_scale')
+output_score = recipe_config.get('output_score', False)
+output_sentiment = recipe_config.get('output_sentiment', True)
 output_probabilities = recipe_config.get('output_confidence', False)
 
 
@@ -57,58 +59,43 @@ CHUNK_SIZE = 10000
 logger.info("Start chunk-processing of input dataset.".format(dataset_name))
 
 n_lines = 0
-for chunk_idx, df in enumerate(input_dataset.iter_dataframes(chunksize=CHUNK_SIZE)):
 
-    # TODO output the initial text, not the clean ones
-    texts = df[text_column_name].apply(lambda s: clean_text(str(s)).decode('utf-8')).values
-    predicted_scores, confidence_list = model.predict(list(texts))
+with output_dataset.get_writer() as writer:
+    for chunk_idx, df in enumerate(input_dataset.iter_dataframes(chunksize=CHUNK_SIZE)):
 
-    # Post-process predicted Sentiment
-    predicted_scores = np.array([int(v[0].split('__')[-1]) for v in predicted_scores])
-    if sentiment_scale == 'binary':
-        predicted_scores += -1  # polarity model predicts 1/2 instead of 0/1
-    # TODO fix that, confidence_list is a list, not an np array, maybe fasttext update broke stuff...
-    # confidence_list = confidence_list.ravel()
+        # TODO output the initial text, not the clean ones
+        texts = df[text_column_name].apply(lambda s: clean_text(str(s)).decode('utf-8')).values
+        predicted_scores, confidence_list = model.predict(list(texts))
 
-    if chunk_idx==0:
-        # Compute new column names
-        new_cols = ["predicted_scores", "predicted_sentiment"]
+        # Post-process predicted Sentiment
+        predicted_scores = np.array([int(v[0].split('__')[-1]) for v in predicted_scores])
+        if sentiment_scale == 'binary':
+            predicted_scores += -1  # polarity model predicts 1/2 instead of 0/1
+
+        if chunk_idx==0:
+            new_cols = ["predicted_score", "predicted_sentiment", "prediction_confidence"]
+            for i, column in enumerate(new_cols):
+                if column in df.columns:
+                    j = 1
+                    while column + "_{}".format(j) in df.columns:
+                        j += 1
+                    new_cols[i] = column + "_{}".format(j)
+
+        # Add prediction to output dataframe
+        if output_score:
+            df[new_cols[0]] = predicted_scores
+        if output_sentiment:
+            if sentiment_scale == 'binary':
+                df[new_cols[1]] = ["positive" if p==1 else "negative" for p in predicted_scores]
+            else:
+                df[new_cols[1]] = [ "highly negative" if p==1 else "negative" if p==2 else "neutral" if p==3 else "positive" if p==4 else "highly positive" if p==5 else "" for p in predicted_scores]
         if output_probabilities:
-            new_cols.append("prediction_confidence")
+            df[new_cols[2]] = np.array(confidence_list) / float(max(confidence_list))
 
-        for i, column in enumerate(new_cols):
-            if column in df.columns:
-                j = 1
-                while column + "_{}".format(j) in df.columns:
-                    j += 1
-                new_cols[i] = column + "_{}".format(j)
-
-    # Add prediction to output dataframe
-    df[new_cols[0]] = predicted_scores
-    if sentiment_scale == 'binary':
-        df[new_cols[1]] = [
-            "positive" if p==1
-            else "negative" for p in predicted_scores]
-    else:
-        df[new_cols[1]] = [
-            "highly negative" if p==1
-            else "negative" if p==2
-            else "neutral" if p==3
-            else "positive" if p==4
-            else "highly positive" if p==5
-            else "" for p in predicted_scores]
-
-    if output_probabilities:
-        df[new_cols[2]] = np.array(confidence_list) / float(max(confidence_list))
-
-    # Append dataframe to output Dataset
-    if chunk_idx==0:
-        output_dataset.write_schema_from_dataframe(df)
-        writer = output_dataset.get_writer()
-        writer.write_dataframe(df)
-    else:
+        # Append dataframe to output Dataset
+        if chunk_idx==0:
+            output_dataset.write_schema_from_dataframe(df)
         writer.write_dataframe(df)
 
-    n_lines += len(df)
-    logger.info("Finished processing {} lines".format(n_lines))
-writer.close()
+        n_lines += len(df)
+        logger.info("Finished processing {} lines".format(n_lines))
