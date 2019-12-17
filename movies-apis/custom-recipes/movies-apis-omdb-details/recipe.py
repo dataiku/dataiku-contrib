2782a5322e3b89c3e98de7f641c72fd7cdc11131
Du Phan <dphan@MacBook-Pro.local>
2019-12-17 12:18:24 +0100

    support for py3, update api url, fix bug with empty rows

--- movies-apis/custom-recipes/movies-apis-omdb-details/recipe.py
+++ movies-apis/custom-recipes/movies-apis-omdb-details/recipe.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import requests
+import logging
 import dataiku
 from dataiku.customrecipe import *
 
+logger = logging.getLogger(__name__)
+
 input_dataset = dataiku.Dataset(get_input_names_for_role('input_dataset')[0])
 
+api_key = get_plugin_config().get('omdb_api_key')
+if api_key is None:
+    raise ValueError('You need to add an api key in the plugin setting.')
 lookup_col = get_recipe_config().get('title_col','')
 lookup_name = 'title_queried'
 if lookup_col != '':
@@ -16,7 +22,7 @@ else:
     if lookup_col == '':
         raise Exception('Please provide either a column containing titles or a column containing IMDb ids.')
 
-base_query = 'http://www.omdbapi.com/?' \
+base_query = 'https://www.omdbapi.com/?apikey={api_key}&' \
     + "tomatoes=true" \
     + {
         "all"    : "",
@@ -26,11 +32,13 @@ base_query = 'http://www.omdbapi.com/?' \
       }[get_recipe_config()['type']]
     # y year of relase, plot={short,full}
 
+base_query = base_query.format(api_key=api_key)
+    
 output_dataset = dataiku.Dataset(get_output_names_for_role('output_dataset')[0])
 output_writer = output_dataset.get_writer()
 
 def write_output_schema(sample_line):
-    print "setting schema"
+    logger.info("setting schema")
     output_schema = [
         {'name':lookup_name,         'type':'string'},
         {'name':'Title',             'type':'string'},
@@ -38,16 +46,16 @@ def write_output_schema(sample_line):
         {'name':'imdbRating',        'type':'double'},
         {'name':'imdbVotes',         'type':'bigint'},
         {'name':'Metascore',         'type':'bigint'},
-        {'name':'tomatoConsensus',   'type':'string'},
-        {'name':'tomatoFresh',       'type':'bigint'},
-        {'name':'tomatoImage',       'type':'string'},
-        {'name':'tomatoMeter',       'type':'bigint'},
-        {'name':'tomatoRating',      'type':'double'},
-        {'name':'tomatoReviews',     'type':'bigint'},
-        {'name':'tomatoRotten',      'type':'bigint'},
-        {'name':'tomatoUserMeter',   'type':'bigint'},
-        {'name':'tomatoUserRating',  'type':'double'},
-        {'name':'tomatoUserReviews', 'type':'bigint'},
+        #{'name':'tomatoConsensus',   'type':'string'},
+        #{'name':'tomatoFresh',       'type':'bigint'},
+        #{'name':'tomatoImage',       'type':'string'},
+        #{'name':'tomatoMeter',       'type':'bigint'},
+        #{'name':'tomatoRating',      'type':'double'},
+        #{'name':'tomatoReviews',     'type':'bigint'},
+        #{'name':'tomatoRotten',      'type':'bigint'},
+        #{'name':'tomatoUserMeter',   'type':'bigint'},
+        #{'name':'tomatoUserRating',  'type':'double'},
+        #{'name':'tomatoUserReviews', 'type':'bigint'},
         {'name':'Actors',
             "type":"array", "timestampNoTzAsDate": False, "maxLength": -1,
             "arrayContent": {"type": "string", "timestampNoTzAsDate": False, "maxLength": 1000}},
@@ -84,48 +92,53 @@ def write_output_schema(sample_line):
         if key not in known_keys:
             output_schema.append({'name':key, 'type':'string'})
     output_dataset.write_schema(output_schema)
-output_schema_set = output_dataset.read_schema(raise_if_empty=False) != []
+output_schema_set = False #output_dataset.read_schema(raise_if_empty=False) != []
 
 results_notFound = []
-for row in input_dataset.iter_rows(log_every=10):
-    lookup = row[lookup_col]
-    print "looking up", lookup.encode('utf-8')
-    query = base_query + ('&i=' if use_id else '&t=') + lookup.encode('utf-8')
-    movie = requests.get(query).json()
-    if movie['Response'] == 'True':
-        # some obvious cleaning:
-        del movie['Response']
-        movie['imdbVotes'] = movie['imdbVotes'].replace(',','')
-        for col in ['Actors', 'Country', 'Director', 'Genre', 'Language', 'Writer']:
-            movie[col] = '["' + movie[col].replace(', ','","') + '"]'
-        if movie['Runtime'].endswith(' min'):
-            movie['Runtime'] = movie['Runtime'][:-len(' min')]
-        for col in ['Poster', 'Website', 'tomatoConsensus', 'tomatoImage']:
-            if movie[col] == 'N/A': del movie[col]
-        for col in ['Metascore', 'Runtime', 'Year', 'imdbVotes',
-                    'tomatoFresh', 'tomatoMeter', 'tomatoReviews', 'tomatoRotten',
-                    'tomatoUserMeter', 'tomatoUserReviews']:
-            try:
-                if movie[col] == 'N/A': del movie[col]
-                else: movie[col] = int(movie[col])
-            except:
-                print "cannot cast to int:", col, movie[col]
-        for col in ['imdbRating', 'tomatoRating', 'tomatoUserRating']:
-            try:
-                if movie[col] == 'N/A': del movie[col]
-                else: movie[col] = float(movie[col])
-            except:
-                print "cannot cast to float:", col, movie[col]
 
-        movie[lookup_name] = row[lookup_col]
-        if not output_schema_set:
-            write_output_schema(movie)
-            output_schema_set = True
-        output_writer.write_row_dict(movie)
-    else:
-        print 'Error'
-        results_notFound.append({lookup_name: lookup, 'error': movie['Error']})
-        assert movie['Response'] == 'False'
+for row in input_dataset.iter_rows():
+    lookup = row[lookup_col]
+    if lookup is not None:
+        logger.info("looking up {}".format(lookup))
+        query = base_query + ('&i=' if use_id else '&t=') + lookup#.encode('utf-8')
+        print(')))))))', query)
+        movie = requests.get(query).json()
+        if movie['Response'] == 'True':
+            # some obvious cleaning:
+            del movie['Response']
+            movie['imdbVotes'] = movie['imdbVotes'].replace(',','')
+            for col in ['Actors', 'Country', 'Director', 'Genre', 'Language', 'Writer']:
+                movie[col] = '["' + movie[col].replace(', ','","') + '"]'
+            if movie['Runtime'].endswith(' min'):
+                movie['Runtime'] = movie['Runtime'][:-len(' min')]
+            for col in ['Poster', 'Website', 'tomatoConsensus', 'tomatoImage']:
+                if movie[col] == 'N/A': del movie[col]
+            for col in ['Metascore', 'Runtime', 'Year', 'imdbVotes',
+                        'tomatoFresh', 'tomatoMeter', 'tomatoReviews', 'tomatoRotten',
+                        'tomatoUserMeter', 'tomatoUserReviews']:
+                try:
+                    if movie[col] == 'N/A': del movie[col]
+                    else: movie[col] = int(movie[col])
+                except:
+                    logger.info("cannot cast to int:", col, movie[col])
+            for col in ['imdbRating', 'tomatoRating', 'tomatoUserRating']:
+                try:
+                    if movie[col] == 'N/A': del movie[col]
+                    else: movie[col] = float(movie[col])
+                except:
+                    logger.info("cannot cast to float:", col, movie[col])
+
+            movie[lookup_name] = row[lookup_col]
+            if not output_schema_set:
+                write_output_schema(movie)
+                output_schema_set = True
+            movie_final = {k:v for k,v in movie.items() if 'tomato' not in k}
+            print('))))))))))))))', movie_final)
+            output_writer.write_row_dict(movie_final)
+        else:
+            logger.info('Error')
+            results_notFound.append({lookup_name: lookup, 'error': movie['Error']})
+            assert movie['Response'] == 'False'
 output_writer.close()
 
 if get_output_names_for_role('movies_not_found'):
