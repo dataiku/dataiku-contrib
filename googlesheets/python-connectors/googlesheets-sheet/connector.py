422471b906f1e53e3e19fa57cb09a722246e784e
Jeremy <jeremy.greze@dataiku.com>
2020-04-02 18:25:58 +0200

    gspread's worksheet object initianalized in external module

--- googlesheets/python-connectors/googlesheets-sheet/connector.py
+++ googlesheets/python-connectors/googlesheets-sheet/connector.py
@@ -1,11 +1,9 @@
 from dataiku.connector import Connector, CustomDatasetWriter
 import json
-import os.path
 from collections import OrderedDict
-import gspread
 from gspread.utils import rowcol_to_a1
-from oauth2client.service_account import ServiceAccountCredentials
 from slugify import slugify
+from googlesheets import get_spreadsheet
 
 class MyConnector(Connector):
 
@@ -16,48 +14,6 @@ class MyConnector(Connector):
         self.tab_id = self.config.get("tab_id")
         self.result_format = self.config.get("result_format")
         self.list_unique_slugs = []
-
-        file = self.credentials.splitlines()[0]
-        if os.path.isfile(file):
-            try:
-                with open(file, 'r') as f:
-                    self.credentials  = json.load(f)
-                    f.close()
-            except Exception as e:
-                raise ValueError("Unable to read the JSON Service Account from file '%s'.\n%s" % (file, e))
-        else:
-            try:
-                self.credentials  = json.loads(self.credentials)
-            except Exception as e:
-                raise Exception("Unable to read the JSON Service Account.\n%s" % e)
-
-        scope = [
-            'https://www.googleapis.com/auth/spreadsheets',
-            #'https://www.googleapis.com/auth/drive'
-        ]
-        credentials = ServiceAccountCredentials.from_json_keyfile_dict(self.credentials, scope)
-        self.gspread_client = gspread.authorize(credentials)
-
-
-    def get_spreadsheet(self):
-
-        try:
-            return self.gspread_client.open_by_key(self.doc_id).worksheet(self.tab_id)
-        except gspread.exceptions.SpreadsheetNotFound as e:
-            raise Exception("Trying to open non-existent or inaccessible spreadsheet document.")
-        except gspread.exceptions.WorksheetNotFound as e:
-            raise Exception("Trying to open non-existent sheet. Verify that the sheet name exists (%s)." % self.tab_id)
-        except gspread.exceptions.APIError as e:
-            if hasattr(e, 'response'):
-                error_json = e.response.json()
-                print(error_json)
-                error_status = error_json.get("error", {}).get("status")
-                email = self.credentials.get("client_email", "(email missing)")
-                if error_status == 'PERMISSION_DENIED':
-                    raise Exception("The Service Account does not have permission to read or write on the spreadsheet document. Have you shared the spreadsheet with %s?" % email)
-                if error_status == 'NOT_FOUND':
-                    raise Exception("Trying to open non-existent spreadsheet document. Verify the document id exists (%s)." % self.doc_id)
-            raise Exception("The Google API returned an error: %s" % e)
 
 
     def get_unique_slug(self, string):
@@ -91,7 +47,7 @@ class MyConnector(Connector):
 
         The dataset schema and partitioning are given for information purpose.
         """
-        ws = self.get_spreadsheet()
+        ws = get_spreadsheet(self.credentials, self.doc_id, self.tab_id)
         rows = ws.get_all_values()
         columns = rows[0]
         columns_slug = list(map(self.get_unique_slug, columns))
@@ -181,7 +137,7 @@ class MyCustomDatasetWriter(CustomDatasetWriter):
 
 
     def flush(self):
-        ws = self.parent.get_spreadsheet()
+        ws = get_spreadsheet(self.parent.credentials, self.parent.doc_id, self.parent.tab_id)
 
         num_columns = len(self.buffer[0])
         num_lines = len(self.buffer)
