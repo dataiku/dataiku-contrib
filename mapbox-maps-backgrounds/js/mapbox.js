3373870355340c75f87ca46c7f5e60d5cb6d1f2a
HENRI CHABERT <chabhenrib@gmail.com>
2020-07-07 13:28:57 +0200

    Removed api call to plugins.get

--- mapbox-maps-backgrounds/js/mapbox.js
+++ mapbox-maps-backgrounds/js/mapbox.js
@@ -1,48 +1,42 @@
 (function() {
     const pluginDesc = window.dkuAppConfig.loadedPlugins.find(d => d.id === 'mapbox-maps-backgrounds');
 
-    DataikuAPI.plugins.get("mapbox-maps-backgrounds").success(function(data) {
-        if (!data || !data.settings) {
-            console.info("Mapbox Plugin: no settings, bailing out");
-            return;
-        }
-        
-        var pluginSettings = data.settings.config;
-        
-        var accessToken = pluginSettings.accessToken;
+    if (!pluginDesc || !pluginDesc.params) {
+        console.info("Mapbox Plugin: no settings, bailing out");
+        return;
+    }
 
-        if (!accessToken) {
-            console.info("Mapbox: no access token, bailing out");
-            return;
-        }
+    var pluginSettings = pluginDesc.params;
 
-        var allStyles = []
+    var accessToken = pluginSettings.accessToken;
 
-        if (pluginSettings.addDefaultStyles) {
-            var defaultStyles =  [
-                ['mapbox.satellite', "Mapbox Satellite"],
-                ['mapbox.mapbox-streets-v8', "Mapbox Streets v8"],
-                ['mapbox.mapbox-streets-v7', "Mapbox Streets v7"],
-                ['mapbox.mapbox-streets-v6', "Mapbox Streets v6"],
-                ['mapbox.mapbox-streets-v5', "Mapbox Streets v5"],
-                ['mapbox.mapbox-terrain-v2', "Mapbox Terrain v2"],
-                ['mapbox.mapbox-traffic-v1', "Mapbox Traffic v1"]
-            ]
-            allStyles = allStyles.concat(defaultStyles)
-        }
+    if (!accessToken) {
+        console.info("Mapbox: no access token, bailing out");
+        return;
+    }
 
-        if (pluginSettings.additionalStyles) {
-            allStyles = allStyles.concat(
-                    pluginSettings.additionalStyles.split("\n").filter(x => x.length > 0).map(x => [x.split(" ")[0], x.split(" ")[1]])
-                )
-        }
+    var allStyles = []
 
-        allStyles.forEach(function(bg) {
-            dkuMapBackgrounds.addMapbox(bg[0], bg[1], accessToken);
-        });
+    if (pluginSettings.addDefaultStyles) {
+        var defaultStyles =  [
+            ['mapbox.satellite', "Mapbox Satellite"],
+            ['mapbox.mapbox-streets-v8', "Mapbox Streets v8"],
+            ['mapbox.mapbox-streets-v7', "Mapbox Streets v7"],
+            ['mapbox.mapbox-streets-v6', "Mapbox Streets v6"],
+            ['mapbox.mapbox-streets-v5', "Mapbox Streets v5"],
+            ['mapbox.mapbox-terrain-v2', "Mapbox Terrain v2"],
+            ['mapbox.mapbox-traffic-v1', "Mapbox Traffic v1"]
+        ]
+        allStyles = allStyles.concat(defaultStyles)
+    }
 
-    }).error(function(a, b, c) {
-        console.error("Failed to get MapBox plugin settings", getErrorDetails(a, b, c));
+    if (pluginSettings.additionalStyles) {
+        allStyles = allStyles.concat(
+                pluginSettings.additionalStyles.split("\n").filter(x => x.length > 0).map(x => [x.split(" ")[0], x.split(" ")[1]])
+            )
+    }
+
+    allStyles.forEach(function(bg) {
+        dkuMapBackgrounds.addMapbox(bg[0], bg[1], accessToken);
     });
-    
 })();
