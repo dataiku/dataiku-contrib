b38f5b7ed33c6236e999ede490c40277372a8681
Gonzalo Betegon <43414030+gbetegon88@users.noreply.github.com>
2021-12-20 11:50:55 -0500

    Add condition to append_datasets_to_list()
    Doesn't add a dataset to the list if it's already in the list

--- clear-intermediate-datasets/python-lib/utils.py
+++ clear-intermediate-datasets/python-lib/utils.py
@@ -6,4 +6,4 @@ def populate_result_table_with_list():
 def append_datasets_to_list(recipe_dict, list_to_append):
     # Append datasets' names from recipe's input or output dictionaries to an input or output list respectively
     for key in recipe_dict:
-        list_to_append += [x["ref"] for x in recipe_dict[key]["items"]]
+        list_to_append += [x["ref"] for x in recipe_dict[key]["items"] if x["ref"] not in list_to_append]
