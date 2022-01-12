d8fe3fad5170c88e6f527c525994d6127f452b07
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-12 20:25:52 +0800

    Delete instagram directory

--- instagram/js/instagram.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('instagram.module', []);
-
-app.controller('UsersController', function($scope) {
-    // Placeholder if we want to add more advanced behavior to the plugin
-});
-
-app.controller('UsersMediaController', function($scope) {
-    // Placeholder if we want to add more advanced behavior to the plugin
-});
