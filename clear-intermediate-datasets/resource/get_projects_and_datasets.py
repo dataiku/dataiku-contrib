43088c44917308035c01841c8e82f0bf5f5be3d7
Gonzalo Betegon <43414030+gbetegon88@users.noreply.github.com>
2022-07-14 10:30:13 -0400

    updated get_projects_and_datasets.py
    - condensed repeated lines in function in utils.py
    - simplified if looping

--- clear-intermediate-datasets/resource/get_projects_and_datasets.py
+++ clear-intermediate-datasets/resource/get_projects_and_datasets.py
@@ -1,29 +1,22 @@
 import dataiku
+from utils import append_dropdown_choices
+
 def do(payload, config, plugin_config, inputs):
     """
-    Create list of options of projects in instance.
+    Populate choices for projects and datasets in the initial macro interface
     """
     client = dataiku.api_client()
+    
     # Set project dropdown
     if payload.get('parameterName') == "project_key":
         projects = client.list_projects()
-        choices = []
-        for project in projects:
-            choices.append({"value": project.get('projectKey'), "label": project.get('projectKey')})
-        return {"choices": choices}
-    # Set dataset multiselect box - Display list of datasets of current project if no project has been selected
-    elif payload.get('parameterName') == "datasets_to_exclude" and config.get("project_key", None) == None:
-        project = client.get_default_project()
-        datasets = project.list_datasets()
-        choices = []
-        for dataset in datasets:
-            choices.append({"value": dataset.get('name'), "label": dataset.get('name')})
-        return {"choices": choices}
-    # Set dataset multiselect box - Display list of datasets if a project has been selected
+        return {"choices": append_dropdown_choices(projects, 'projectKey')}
+    
+    # Set dataset multiselect box
     elif payload.get('parameterName') == "datasets_to_exclude":
-        project = client.get_project(config.get("project_key"))
+        if config.get("project_key", None) == None:
+            project = client.get_default_project()
+        else:
+            project = client.get_project(config.get("project_key"))
         datasets = project.list_datasets()
-        choices = []
-        for dataset in datasets:
-            choices.append({"value": dataset.get('name'), "label": dataset.get('name')})
-        return {"choices": choices}    
+        return {"choices": append_dropdown_choices(datasets, 'name')}
