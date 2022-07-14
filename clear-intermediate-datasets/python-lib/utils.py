b4adffd5133e313747c5d29e0f16b162a0f55cb4
Gonzalo Betegon <43414030+gbetegon88@users.noreply.github.com>
2022-07-14 10:26:55 -0400

    Update utils.py adding a function for the runnable.json

--- clear-intermediate-datasets/python-lib/utils.py
+++ clear-intermediate-datasets/python-lib/utils.py
@@ -4,6 +4,18 @@ def populate_result_table_with_list():
     return NotImplementedError()
 
 def append_datasets_to_list(recipe_dict, list_to_append):
-    # Append datasets' names from recipe's input or output dictionaries to an input or output list respectively
+    """
+    Append datasets' names from recipe's input or output dictionaries to an input or output list respectively
+    """
     for key in recipe_dict:
         list_to_append += [x["ref"] for x in recipe_dict[key]["items"] if x["ref"] not in list_to_append]
+
+def append_dropdown_choices(list_to_parse, key_to_use):
+    """
+    Parses through the list of projects in an instance, or a list of datasets in a project
+    to return the list as choices of a dropdown in the runnable.json
+    """
+    choices = []
+    for item in list_to_parse:
+        choices.append({"value": item.get(key_to_use), "label": item.get(key_to_use)})
+    return choices
